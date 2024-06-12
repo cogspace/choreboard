1fc573cd114d5c42bf6f82ae00e0ca74ddc73be5
Jon Molnar <jon@cogspace.com>
2024-06-12 09:52:04 -0700

    fixes, new features, gitignore

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const { Sequelize, DataTypes } = require('sequelize')
 
 const app = express()
@@ -8,42 +8,54 @@ const port = 3000
 const sequelize = new Sequelize('sqlite:boards.db')
 
 const Board = sequelize.define('Board', {
-	id: DataTypes.STRING
+	id: {
+		type: DataTypes.STRING,
+		primaryKey: true
+	}
 })
 
 const Player = sequelize.define('Player', {
-	boardId: DataTypes.STRING,
-	name: DataTypes.STRING,
-	color: DataTypes.STRING,
-	points: DataTypes.INTEGER
+	boardId: { type: DataTypes.STRING, allowNull: false},
+	name: { type: DataTypes.STRING, allowNull: false},
+	color: { type: DataTypes.STRING, allowNull: false},
+	points: { type: DataTypes.INTEGER, allowNull: false},
 })
 
 const Chore = sequelize.define('Chore', {
-	boardId: DataTypes.STRING,
-	name: DataTypes.STRING,
-	points: DataTypes.INTEGER
+	boardId: { type: DataTypes.STRING, allowNull: false},
+	name: { type: DataTypes.STRING, allowNull: false},
+	points: { type: DataTypes.INTEGER, allowNull: false},
 })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
 	res.render('pages/index')
 })
 
-app.get('/:boardId', await (req, res) => {
-	const { boardId } = req.params;
-	const board = await Board.findByPk(boardId);
+app.get('/b/:boardId', async (req, res) => {
+	await sequelize.sync()
+	const { boardId } = req.params
+	const board = await Board.findByPk(boardId)
 	const players = await Player.findAll({ where: { boardId }})
 	const chores = await Chore.findAll({ where: { boardId }})
 
 	res.render('pages/board', { players, chores })
 })
 
-app.post('/', (req, res) => {
-
+app.post('/b', async (req, res) => {
+	await sequelize.sync()
+	const id = crypto.randomUUID()
+	const board = await Board.create({ id })
+	for (let i = 0; i < 10; i++) {
+		const player = req.body[`player${i}`]
+		if (player) {
+			await Player.create({ boardId: id, name: player, color: 'gray', points: 0 })
+		}
+	}
 })
 
 app.listen(port, () => {
