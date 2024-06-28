771378247648f20caa843058143bc118ad9d3e3c
Jon Molnar <jon@cogspace.com>
2024-06-28 09:11:08 -0700

    fix host

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const port = process.env.PORT || 3000
 
 const sequelize = new Sequelize({
 	dialect: 'mysql',
+	host: process.env.DB_HOST || '127.0.0.1',
 	database: process.env.DB_DATABASE || 'choreboard',
 	username: process.env.DB_USERNAME || 'choreboard',
 	password: process.env.DB_PASSWORD || 'password',
