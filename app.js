f8c96587abe0a0879f2ae6197b744c9eb2f6a90f
Jon Molnar <jon@cogspace.com>
2024-06-28 08:24:53 -0700

    many improvements lol

--- app.js
+++ app.js
@@ -3,10 +3,17 @@ const crypto = require('node:crypto')
 const bodyParser = require('body-parser')
 const { Sequelize, DataTypes } = require('sequelize')
 
-const app = express()
-const port = 3000
+const randomID = () => crypto.randomBytes(9).toString('base64url')
 
-const sequelize = new Sequelize('sqlite:boards.db')
+const app = express()
+const port = process.env.PORT || 3000
+
+const sequelize = new Sequelize({
+	dialect: 'mysql',
+	database: process.env.DB_DATABASE || 'choreboard',
+	username: process.env.DB_USERNAME || 'choreboard',
+	password: process.env.DB_PASSWORD || 'password',
+})
 
 const Board = sequelize.define('Board', {
 	id: { type: DataTypes.STRING, primaryKey: true },
@@ -34,9 +41,15 @@ app.use(bodyParser.json())
 
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
-	res.render('index')
-})
+function getAllPlayers(boardId) {
+	return Player.findAll({ where: { boardId }, order: [['name', 'ASC']] })
+}
+
+function getAllChores(boardId) {
+	return Chore.findAll({ where: { boardId }, order: [['name', 'ASC']] })
+}
+
+// BOARDS
 
 app.get('/boards/:boardId', async (req, res) => {
 	const { boardId } = req.params
@@ -44,33 +57,26 @@ app.get('/boards/:boardId', async (req, res) => {
 	if (!board) {
 		res.render('index')
 	}
-	const players = await Player.findAll({ where: { boardId } })
-	const chores = await Chore.findAll({ where: { boardId } })
+	const players = await getAllPlayers(boardId)
+	const chores = await getAllChores(boardId)
 	res.render('board', { board, players, chores })
 })
 
 app.post('/boards', async (req, res) => {
-	const id = crypto.randomUUID()
+	const id = randomID()
 	await Board.create({ id })
 	res.redirect(`/boards/${id}`)
 })
 
+// PLAYERS
+
 app.post('/boards/:boardId/players', async (req, res) => {
-	const id = crypto.randomUUID()
+	const id = randomID()
 	const { boardId } = req.params
 	const { name, color } = req.body
 	await Player.create({ id, boardId, name, color, points: 0 })
-	const players = await Player.findAll({ where: { boardId } })
-	res.render('manage-players-list', { players })
-})
-
-app.post('/boards/:boardId/chores', async (req, res) => {
-	const id = crypto.randomUUID()
-	const { boardId } = req.params
-	const { name, points } = req.body
-	await Chore.create({ id, boardId, name, points })
-	const chores = await Chore.findAll({ where: { boardId } })
-	res.render('manage-chores-list', { chores })
+	const players = await getAllPlayers(boardId)
+	res.render('update-players', { players })
 })
 
 app.put('/manage-players/:playerId', async (req, res) => {
@@ -78,41 +84,16 @@ app.put('/manage-players/:playerId', async (req, res) => {
 	const { color, name, points } = req.body
 	const player = await Player.findByPk(playerId)
 	await player.update({ color, name, points })
-	const players = await Player.findAll({ where: { boardId: player.boardId } })
-	res.render('manage-players-list', { players })
-})
-
-app.put('/manage-chores/:choreId', async (req, res) => {
-	const { choreId } = req.params
-	const { name, points } = req.body
-	const chore = await Chore.findByPk(choreId)
-	await chore.update({ name, points })
-	const chores = await Chore.findAll({ where: { boardId: chore.boardId } })
-	res.render('manage-chores-list', { chores })
+	const players = await getAllPlayers(player.boardId)
+	res.render('update-players', { players })
 })
 
 app.delete('/manage-players/:playerId', async (req, res) => {
 	const { playerId } = req.params
 	const player = await Player.findByPk(playerId)
 	await player.destroy();
-	const players = await Player.findAll({ where: { boardId: player.boardId } })
-	res.render('manage-players-list', { players })
-})
-
-app.delete('/manage-chores/:choreId', async (req, res) => {
-	const { choreId } = req.params
-	const chore = await Chore.findByPk(choreId)
-	await chore.destroy();
-	const chores = await Chore.findAll({ where: { boardId: chore.boardId } })
-	res.render('manage-chores-list', { chores })
-})
-
-app.post('/chores', async (req, res) => {
-	const id = crypto.randomUUID()
-	const { boardId, name, points } = req.body
-	await Chore.create({ id, boardId, name, points })
-	const chores = await Chore.findAll({ where: { boardId } })
-	res.render('chores-list', { chores })
+	const players = await getAllPlayers(player.boardId)
+	res.render('update-players', { players })
 })
 
 app.post('/do-chore', async (req, res) => {
@@ -120,27 +101,50 @@ app.post('/do-chore', async (req, res) => {
 	const chore = await Chore.findByPk(choreId)
 	const player = await Player.findByPk(playerId)
 	await player.update({ points: player.points + chore.points })
-	const players = await Player.findAll({ where: { boardId: player.boardId } })
-	res.render('players', { players })
+	const players = await getAllPlayers(player.boardId)
+	res.render('update-players', { players })
 })
 
-app.get('/boards/:boardId/players', async (req, res) => {
+// CHORES
+
+app.post('/boards/:boardId/chores', async (req, res) => {
+	const id = randomID()
 	const { boardId } = req.params
-	const players = await Player.findAll({ where: { boardId } })
-	res.render('players', { players })
+	const { name, points } = req.body
+	await Chore.create({ id, boardId, name, points })
+	const chores = await getAllChores(boardId)
+	res.render('update-chores', { chores })
 })
 
-app.get('/boards/:boardId/chores', async (req, res) => {
-	const { boardId } = req.params
-	const chores = await Chore.findAll({ where: { boardId } })
-	res.render('chores-list', { chores })
+app.put('/manage-chores/:choreId', async (req, res) => {
+	const { choreId } = req.params
+	const { name, points } = req.body
+	console.log(points, 'is a', typeof points)
+	console.log(req.body)
+	const chore = await Chore.findByPk(choreId)
+	await chore.update({ name, points })
+	const chores = await getAllChores(chore.boardId)
+	res.render('update-chores', { chores })
 })
 
-app.get('/node_modules/**', async (req, res) => {
+app.delete('/manage-chores/:choreId', async (req, res) => {
+	const { choreId } = req.params
+	const chore = await Chore.findByPk(choreId)
+	await chore.destroy();
+	const chores = await getAllChores(chore.boardId)
+	res.render('update-chores', { chores })
+})
+
+// STATIC + ROOT
+
+app.get('/static/**', async (req, res) => {
 	res.sendFile(req.path, { root: __dirname })
+})
+
+app.get('/', (req, res) => {
+	res.render('index')
 })
 
 app.listen(port, () => {
 	console.log(`Server is listening on port ${port}`)
 })
-
